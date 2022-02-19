1384e488d9fb422cb733a9f76e8a28094793c68c
rohshar6 <rohshar6@cisco.com>
2022-02-19 18:08:19 +0530

    project set-up complete

---
+++ iOS Netflix clone/MainTabBarViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
